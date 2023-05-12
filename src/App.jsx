41f9ba368838fe40d559a08bb46dba98219d7545
Kimber Ogden <starfleet@Kimbers-MacBook-Pro.local>
2023-05-12 10:47:41 -0500

    useEffect to call function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 function Header() {
@@ -68,7 +68,7 @@ function Content() {
 
     })
   }
-
+  useEffect(handleIndexPosts, [])
   return (
     <div>
       <PostsNew />
