de7e69dfd02f2edb1284a8e99dcd778a78e01abf
Kimber Ogden <starfleet@Kimbers-MacBook-Pro.local>
2023-05-11 23:46:46 -0500

    init

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,12 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev">
+        <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev">
+        <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <p>Making changes!</p>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
