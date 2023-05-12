f03c711a0048e0652a9a2e9a61daad891c57bb6f
Kimber Ogden <starfleet@Kimbers-MacBook-Pro.local>
2023-05-12 09:43:54 -0500

    added multiple functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,70 @@
-function App() {
+function Header() {
+  return (
+    <header>
+      <a href="#">Home</a> | <a href="#recipes-index">All posts</a> | <a href="#recipes-new">New post</a>
+    </header>
+  )
+}
+
+function PostsNew() {
+  return (
+    <div id="posts-new">
+      <h1>New recipe</h1>
+      <form>          
+        <div>
+          <p>Title: <input type="text" /></p>
+        </div>
+        <div>
+          <p>Body: <input type="text" /></p>
+        </div>
+        <div>
+          <p>Image: <input type="text" /></p>
+        </div>
+        <button type="submit">Create New Blog</button>
+      </form>
+    </div>
+  )
+}
+
+function PostsIndex() {
+  return (
+    <div id="posts-index">
+      <h1>All blogs</h1>
+      <div className="posts">
+        <h2>Captain's Log</h2>
+        <img src="https://i.imgur.com/Ue42Eeq.png" alt="" />
+        <p>Author: Kimber Ogden</p>
+        <button>More info</button>
+      </div>
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright 2022</p>
+    </footer>
+  )
+}
+
+function Content() {
   return (
     <div>
-      <header>
-        <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
-      </header>
+      <PostsNew />
+      <PostsIndex />
+    </div>
 
-      <div id="posts-new">
-        <h1>New post</h1>
-        <form>
-          <div>
-            <p>Title: <input type="text" /></p>
-            <p>Body: <input type="text" /></p>
-            <p>Image: <input type="text"/></p>
-          </div>
-          <button type="submit">Create post</button>
-        </form>
-      </div>
+  )
+}
 
-      <div id="posts-index">
-        <h1>All posts</h1>
-        <div className="posts">
-          <h2>Captains Blog</h2>
-          <img src="https://i.imgur.com/Ue42Eeq.png" alt="" />
-          <p>Author: Kimber Ogden</p>
-          <button>More info</button>
-        </div>
-        
-      </div>
-
-      <footer>
-        <p>Copyright 2022</p>
-      </footer>
+function App() {
+  return (
+    <div>      
+      <Header />
+      <Content />   
+      <Footer />   
     </div>
   );
 }
-
 export default App;
